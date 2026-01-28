eba790f0563a5bef34fb8b1dea2f945009fa3820
Gabriel Dantas <gabrieldantas1404@icloud.com>
2026-01-27 21:15:53 -0300

    feat: Enhance PersonCreatorController with validation and error handling for person creation

---
+++ src/controllers/person_creator_controller_test.py
@@ -0,0 +1,35 @@
+import pytest
+from .person_creator_controller import PersonCreatorController
+class MockPeopleRepository:
+    def insert_person(self, first_name : str, last_name : str, age : int, pet_id : int):
+        pass
+
+
+def test_create():
+    controller = PersonCreatorController(MockPeopleRepository())
+
+    person_info = {
+        "first_name" : "John",
+        "last_name" : "Doe",
+        "age" : 30,
+        "pet_id" : 4, 
+    }
+
+    response = controller.create(person_info)  
+
+    assert response['data']["type"] == "Person"
+    assert response['data']["count"] == 1
+    assert response['data']["attibutes"] == person_info
+
+def test_create_invalid_person_info():
+    controller = PersonCreatorController(MockPeopleRepository())
+
+    person_info = {
+        "first_name" : "John123",
+        "last_name" : "Doe",
+        "age" : 30,
+        "pet_id" : 4, 
+    }
+    
+    with pytest.raises(Exception):
+        controller.create(person_info)
